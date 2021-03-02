8f32a21f35c88acd9e011e4a6e9865a6aa781229
Carlos Carlos <cayalav17@gmail.com>
2021-03-02 17:34:26 +0000

    Source

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
- 
-def multiplicativeCongruentialMethod(Xo, m, a, randomNums, noOfRandomNums): 
+#https://www.geeksforgeeks.org/linear-congruence-method-for-generating-pseudo-random-numbers/
+
+#https://en.wikipedia.org/wiki/Linear_congruential_generator#:~:text=A%20linear%20congruential%20generator%20(LCG,known%20pseudorandom%20number%20generator%20algorithms.
+
+def linearCongruentialMethod(Xo, m, a, c, randomNums, noOfRandomNums): 
     randomNums[0] = Xo 
     for i in range(1, noOfRandomNums): 
-        randomNums[i] = (randomNums[i - 1] * a) % m 
+        randomNums[i] = ((randomNums[i - 1] * a) +c) % m 
+
 if __name__ == '__main__': 
-    Xo = 3 
-    m = 15 
-    a = 7 
+    Xo = 5 
+    m = 7 
+    a = 3 
+    c = 0
     noOfRandomNums = 10
     randomNums = [0] * (noOfRandomNums) 
-    multiplicativeCongruentialMethod(Xo, m, a, randomNums, noOfRandomNums) 
+    linearCongruentialMethod(Xo, m, a, c,randomNums,noOfRandomNums) 
     for i in randomNums: 
         print(i, end = " ") 
+
+#def lcg(modulus, a, c, seed):
+#  while True:
+#      seed = (a * seed + c) % modulus
+#      yield seed
+#lcg(7,3,2,5)    
